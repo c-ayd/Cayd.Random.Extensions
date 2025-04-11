625651c737783f59a7a03900725bbc1ac5031e93
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-11 23:32:57 +0200

    add random int generator

---
+++ src/Cayd.Random.Extensions/Extensions/NextInt.cs
@@ -0,0 +1,25 @@
+﻿namespace Cayd.Random.Extensions
+{
+    public static partial class RandomExtensions
+    {
+        /// <summary>
+        /// Returns a random 32-bit integer.
+        /// </summary>
+        /// <returns>A 32-bit signed integer that is between <see cref="int.MinValue"/> and <see cref="int.MaxValue"/>.</returns>
+        public static int NextInt(this System.Random random)
+            => random.Next(int.MinValue, int.MaxValue);
+
+        /// <summary>
+        /// Returns a random 32-bit integer within a specified range.
+        /// </summary>
+        /// <param name="minValue">The inclusive lower bound of the random number returned.</param>
+        /// <param name="maxValue">The exclusive lower bound of the random number returned. maxValue must be greater than or equal to minValue.</param>
+        /// <returns>
+        /// A 32-bit signed integer greater than or equal to <paramref name="minValue"/> and less than <paramref name="maxValue"/>; that is, the range of return values includes <paramref name="minValue"/>
+        /// but not <paramref name="maxValue"/>. If minValue equals <paramref name="maxValue"/>, <paramref name="minValue"/> is returned.
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="minValue"/> is greater than <paramref name="maxValue"/>.</exception>
+        public static int NextInt(this System.Random random, int minValue, int maxValue)
+            => random.Next(minValue, maxValue);
+    }
+}
---
+++ test/UnitTests/Cayd.Random.Extensions.Test.Unit/Extensions/NextIntTest.cs
@@ -0,0 +1,31 @@
+﻿namespace Cayd.Random.Extensions.Test.Unit.Extensions
+{
+    public class NextIntTest
+    {
+        [Fact]
+        public void NextInt_WhenRangeIsNotGiven_ShouldReturnValueWithinIntRangeWithSameType()
+        {
+            // Act
+            var result = System.Random.Shared.NextInt();
+
+            // Assert
+            Assert.IsType<int>(result);
+            Assert.True(result >= int.MinValue, $"Result: {result}, Min Value: {int.MinValue}");
+            Assert.True(result <= int.MaxValue, $"Result: {result}, Max Value: {int.MaxValue}");
+        }
+
+        [Theory]
+        [InlineData(-10, 10)]
+        [InlineData(-2147483648, 2147483647)]
+        public void NextInt_WhenRangeIsGiven_ShouldReturnValueWithinRangeWithSameType(int min, int max)
+        {
+            // Act
+            var result = System.Random.Shared.NextInt(min, max);
+
+            // Assert
+            Assert.IsType<int>(result);
+            Assert.True(result >= min, $"Result: {result}, Min Value: {min}");
+            Assert.True(result <= max, $"Result: {result}, Max Value: {max}");
+        }
+    }
+}
