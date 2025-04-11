3cde59b41cc258713fdc18765bb2a542a88ed11c
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-12 00:31:16 +0200

    add random bool generator

---
+++ src/Cayd.Random.Extensions/Extensions/NextBool.cs
@@ -0,0 +1,26 @@
+﻿namespace Cayd.Random.Extensions
+{
+    public static partial class RandomExtensions
+    {
+        /// <summary>
+        /// Returns a random boolean.
+        /// </summary>
+        /// <returns>true with a probability of 0.5.</returns>
+        public static bool NextBool(this System.Random random)
+            => random.Next(0, 2) == 1;
+
+        /// <summary>
+        /// Returns a random boolean with a specified probability.
+        /// </summary>
+        /// <param name="probability">The probability of the result being true. probability must be between 0.0 and 1.0.</param>
+        /// <returns>true with the specified probability.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="probability"/> is not between 0.0 and 1.0.</exception>
+        public static bool NextBool(this System.Random random, double probability)
+        {
+            if (probability < 0.0 || probability > 1.0)
+                throw new ArgumentOutOfRangeException(nameof(probability), probability, "Probability must be between 0.0 and 1.0");
+
+            return random.NextDouble() < probability;
+        }
+    }
+}
---
+++ test/UnitTests/Cayd.Random.Extensions.Test.Unit/Extensions/NextBoolTest.cs
@@ -0,0 +1,63 @@
+﻿namespace Cayd.Random.Extensions.Test.Unit.Extensions
+{
+    public class NextBoolTest
+    {
+        [Fact]
+        public void NextBool_ShouldReturnBoolean()
+        {
+            // Act
+            var result = System.Random.Shared.NextBool();
+
+            // Assert
+            Assert.IsType<bool>(result);
+        }
+
+        [Theory]
+        [InlineData(0.1)]
+        [InlineData(1.0)]
+        public void NextBool_WhenProbabilityIsWithinRange_ShouldReturnBoolean(double probability)
+        {
+            // Act
+            var result = System.Random.Shared.NextBool(probability);
+
+            // Assert
+            Assert.IsType<bool>(result);
+        }
+
+        [Fact]
+        public void NextBool_WhenProbabilityIsZero_ShouldAlwaysReturnFalse()
+        {
+            // Act
+            var result = System.Random.Shared.NextBool(0.0);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void NextBool_WhenProbabilityIsOne_ShouldAlwaysReturnTrue()
+        {
+            // Act
+            var result = System.Random.Shared.NextBool(1.0);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(-0.1)]
+        [InlineData(1.1)]
+        public void NextBool_WhenProbabilityIsNotWithinRange_ShouldReturnBoolean(double probability)
+        {
+            // Act
+            var result = Record.Exception(() =>
+            {
+                System.Random.Shared.NextBool(probability);
+            });
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<ArgumentOutOfRangeException>(result);
+        }
+    }
+}
