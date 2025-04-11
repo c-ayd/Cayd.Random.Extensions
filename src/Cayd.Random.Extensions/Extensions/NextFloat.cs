2c2d369d69a277cda00276e50ca51e31f6ce4b50
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-12 00:37:38 +0200

    add random float generator

---
+++ src/Cayd.Random.Extensions/Extensions/NextFloat.cs
@@ -0,0 +1,25 @@
+﻿namespace Cayd.Random.Extensions
+{
+    public static partial class RandomExtensions
+    {
+        /// <summary>
+        /// Returns a random floating point number.
+        /// </summary>
+        /// <returns>A single precision floating point number that is between <see cref="float.MinValue"/> and <see cref="float.MaxValue"/>.</returns>
+        public static float NextFloat(this System.Random random)
+            => (float)(random.NextDouble() * ((double)float.MaxValue - (double)float.MinValue) + (double)float.MinValue);
+
+        /// <summary>
+        /// Returns a random floating point number within a specified range.
+        /// </summary>
+        /// <param name="minValue">The inclusive lower bound of the random number returned.</param>
+        /// <param name="maxValue">The exclusive lower bound of the random number returned. maxValue must be greater than or equal to minValue.</param>
+        /// <returns>
+        /// A single precision floating point number greater than or equal to <paramref name="minValue"/> and less than <paramref name="maxValue"/>; that is, the range of return values includes <paramref name="minValue"/>
+        /// but not <paramref name="maxValue"/>. If minValue equals <paramref name="maxValue"/>, <paramref name="minValue"/> is returned.
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="minValue"/> is greater than <paramref name="maxValue"/>.</exception>
+        public static float NextFloat(this System.Random random, float minValue, float maxValue)
+            => (float)(random.NextDouble() * ((double)maxValue - (double)minValue) + (double)minValue);
+    }
+}
---
+++ test/UnitTests/Cayd.Random.Extensions.Test.Unit/Extensions/NextFloatTest.cs
@@ -0,0 +1,31 @@
+﻿namespace Cayd.Random.Extensions.Test.Unit.Extensions
+{
+    public class NextFloatTest
+    {
+        [Fact]
+        public void NextFloat_WhenRangeIsNotGiven_ShouldReturnValueWithinFloatRangeWithSameType()
+        {
+            // Act
+            var result = System.Random.Shared.NextFloat();
+
+            // Assert
+            Assert.IsType<float>(result);
+            Assert.True(result >= float.MinValue, $"Result: {result}, Min Value: {float.MinValue}");
+            Assert.True(result <= float.MaxValue, $"Result: {result}, Max Value: {float.MaxValue}");
+        }
+
+        [Theory]
+        [InlineData(-10.0f, 10.0f)]
+        [InlineData(float.MinValue, float.MaxValue)]
+        public void NextFloat_WhenRangeIsGiven_ShouldReturnValueWithinRangeWithSameType(float min, float max)
+        {
+            // Act
+            var result = System.Random.Shared.NextFloat(min, max);
+
+            // Assert
+            Assert.IsType<float>(result);
+            Assert.True(result >= min, $"Result: {result}, Min Value: {min}");
+            Assert.True(result <= max, $"Result: {result}, Max Value: {max}");
+        }
+    }
+}
