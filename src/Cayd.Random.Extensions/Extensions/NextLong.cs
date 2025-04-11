d559c42ea07d93ea8a9af9389c5b4727baf6f685
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-11 23:40:54 +0200

    add random long generator

---
+++ src/Cayd.Random.Extensions/Extensions/NextLong.cs
@@ -0,0 +1,25 @@
+﻿namespace Cayd.Random.Extensions
+{
+    public static partial class RandomExtensions
+    {
+        /// <summary>
+        /// Returns a random 64-bit integer.
+        /// </summary>
+        /// <returns>A 64-bit signed integer that is between <see cref="long.MinValue"/> and <see cref="long.MaxValue"/>.</returns>
+        public static long NextLong(this System.Random random)
+            => random.NextInt64(long.MinValue, long.MaxValue);
+
+        /// <summary>
+        /// Returns a random 64-bit integer within a specified range.
+        /// </summary>
+        /// <param name="minValue">The inclusive lower bound of the random number returned.</param>
+        /// <param name="maxValue">The exclusive lower bound of the random number returned. maxValue must be greater than or equal to minValue.</param>
+        /// <returns>
+        /// A 64-bit signed integer greater than or equal to <paramref name="minValue"/> and less than <paramref name="maxValue"/>; that is, the range of return values includes <paramref name="minValue"/>
+        /// but not <paramref name="maxValue"/>. If minValue equals <paramref name="maxValue"/>, <paramref name="minValue"/> is returned.
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="minValue"/> is greater than <paramref name="maxValue"/>.</exception>
+        public static long NextLong(this System.Random random, long minValue, long maxValue)
+            => random.NextInt64(minValue, maxValue);
+    }
+}
---
+++ test/UnitTests/Cayd.Random.Extensions.Test.Unit/Extensions/NextLongTest.cs
@@ -0,0 +1,31 @@
+﻿namespace Cayd.Random.Extensions.Test.Unit.Extensions
+{
+    public class NextLongTest
+    {
+        [Fact]
+        public void NextLong_WhenRangeIsNotGiven_ShouldReturnValueWithinLongRangeWithSameType()
+        {
+            // Act
+            var result = System.Random.Shared.NextLong();
+
+            // Assert
+            Assert.IsType<long>(result);
+            Assert.True(result >= long.MinValue, $"Result: {result}, Min Value: {long.MinValue}");
+            Assert.True(result <= long.MaxValue, $"Result: {result}, Max Value: {long.MaxValue}");
+        }
+
+        [Theory]
+        [InlineData(-10, 10)]
+        [InlineData(-9223372036854775808, 9223372036854775807)]
+        public void NextLong_WhenRangeIsGiven_ShouldReturnValueWithinRangeWithSameType(long min, long max)
+        {
+            // Act
+            var result = System.Random.Shared.NextLong(min, max);
+
+            // Assert
+            Assert.IsType<long>(result);
+            Assert.True(result >= min, $"Result: {result}, Min Value: {min}");
+            Assert.True(result <= max, $"Result: {result}, Max Value: {max}");
+        }
+    }
+}
