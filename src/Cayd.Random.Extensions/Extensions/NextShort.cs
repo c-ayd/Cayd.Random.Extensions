40b1549b1e2c862050d30169a4f390c687e5f5ce
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-11 23:18:03 +0200

    add random short generator

---
+++ src/Cayd.Random.Extensions/Extensions/NextShort.cs
@@ -0,0 +1,25 @@
+﻿namespace Cayd.Random.Extensions
+{
+    public static partial class RandomExtensions
+    {
+        /// <summary>
+        /// Returns a random 16-bit integer.
+        /// </summary>
+        /// <returns>A 16-bit signed integer that is between <see cref="Int16.MinValue"/> and <see cref="Int16.MaxValue"/>.</returns>
+        public static short NextShort(this System.Random random)
+            => (short)random.Next(Int16.MinValue, Int16.MaxValue);
+
+        /// <summary>
+        /// Returns a random 16-bit integer within a specified range.
+        /// </summary>
+        /// <param name="minValue">The inclusive lower bound of the random number returned.</param>
+        /// <param name="maxValue">The exclusive lower bound of the random number returned. maxValue must be greater than or equal to minValue.</param>
+        /// <returns>
+        /// A 16-bit signed integer greater than or equal to <paramref name="minValue"/> and less than <paramref name="maxValue"/>; that is, the range of return values includes <paramref name="minValue"/>
+        /// but not <paramref name="maxValue"/>. If minValue equals <paramref name="maxValue"/>, <paramref name="minValue"/> is returned.
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="minValue"/> is greater than <paramref name="maxValue"/>.</exception>
+        public static short NextShort(this System.Random random, short minValue, short maxValue)
+            => (short)random.Next(minValue, maxValue);
+    }
+}
---
+++ test/UnitTests/Cayd.Random.Extensions.Test.Unit/Extensions/NextShortTest.cs
@@ -0,0 +1,31 @@
+﻿namespace Cayd.Random.Extensions.Test.Unit.Extensions
+{
+    public class NextShortTest
+    {
+        [Fact]
+        public void NextShort_WhenRangeIsNotGiven_ShouldReturnValueWithinShortRangeWithSameType()
+        {
+            // Act
+            var result = System.Random.Shared.NextShort();
+
+            // Assert
+            Assert.IsType<short>(result);
+            Assert.True(result >= Int16.MinValue, $"Result: {result}, Min Value: {Int16.MinValue}");
+            Assert.True(result <= Int16.MaxValue, $"Result: {result}, Max Value: {Int16.MaxValue}");
+        }
+
+        [Theory]
+        [InlineData(-10, 10)]
+        [InlineData(-32768, 32767)]
+        public void NextShort_WhenRangeIsGiven_ShouldReturnValueWithinRangeWithSameType(short min, short max)
+        {
+            // Act
+            var result = System.Random.Shared.NextShort(min, max);
+
+            // Assert
+            Assert.IsType<short>(result);
+            Assert.True(result >= min, $"Result: {result}, Min Value: {min}");
+            Assert.True(result <= max, $"Result: {result}, Max Value: {max}");
+        }
+    }
+}
