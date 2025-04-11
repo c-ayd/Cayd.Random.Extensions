46e600be6e05b6235a28be5749c41a7f27af0770
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-11 23:29:39 +0200

    add random ushort generator

---
+++ src/Cayd.Random.Extensions/Extensions/NextUShort.cs
@@ -0,0 +1,25 @@
+﻿namespace Cayd.Random.Extensions
+{
+    public static partial class RandomExtensions
+    {
+        /// <summary>
+        /// Returns a non-negative random 16-bit integer.
+        /// </summary>
+        /// <returns>A 16-bit unsigned integer that is between <see cref="ushort.MinValue"/> and <see cref="ushort.MaxValue"/>.</returns>
+        public static ushort NextUShort(this System.Random random)
+            => (ushort)random.Next(ushort.MinValue, ushort.MaxValue);
+
+        /// <summary>
+        /// Returns a non-negative random 16-bit integer within a specified range.
+        /// </summary>
+        /// <param name="minValue">The inclusive lower bound of the random number returned.</param>
+        /// <param name="maxValue">The exclusive lower bound of the random number returned. maxValue must be greater than or equal to minValue.</param>
+        /// <returns>
+        /// A 16-bit unsigned integer greater than or equal to <paramref name="minValue"/> and less than <paramref name="maxValue"/>; that is, the range of return values includes <paramref name="minValue"/>
+        /// but not <paramref name="maxValue"/>. If minValue equals <paramref name="maxValue"/>, <paramref name="minValue"/> is returned.
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="minValue"/> is greater than <paramref name="maxValue"/>.</exception>
+        public static ushort NextUShort(this System.Random random, ushort minValue, ushort maxValue)
+            => (ushort)random.Next(minValue, maxValue);
+    }
+}
---
+++ test/UnitTests/Cayd.Random.Extensions.Test.Unit/Extensions/NextUShortTest.cs
@@ -0,0 +1,31 @@
+﻿namespace Cayd.Random.Extensions.Test.Unit.Extensions
+{
+    public class NextUShortTest
+    {
+        [Fact]
+        public void NextUShort_WhenRangeIsNotGiven_ShouldReturnValueWithinUShortRangeWithSameType()
+        {
+            // Act
+            var result = System.Random.Shared.NextUShort();
+
+            // Assert
+            Assert.IsType<ushort>(result);
+            Assert.True(result >= ushort.MinValue, $"Result: {result}, Min Value: {ushort.MinValue}");
+            Assert.True(result <= ushort.MaxValue, $"Result: {result}, Max Value: {ushort.MaxValue}");
+        }
+
+        [Theory]
+        [InlineData(5, 10)]
+        [InlineData(0, 65535)]
+        public void NextUShort_WhenRangeIsGiven_ShouldReturnValueWithinRangeWithSameType(ushort min, ushort max)
+        {
+            // Act
+            var result = System.Random.Shared.NextUShort(min, max);
+
+            // Assert
+            Assert.IsType<ushort>(result);
+            Assert.True(result >= min, $"Result: {result}, Min Value: {min}");
+            Assert.True(result <= max, $"Result: {result}, Max Value: {max}");
+        }
+    }
+}
