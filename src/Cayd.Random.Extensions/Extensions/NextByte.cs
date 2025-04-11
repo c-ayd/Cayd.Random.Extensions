59cb459167b181df166cf3bbb1b6e16a6894b37a
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-11 23:42:33 +0200

    fix code documentation

--- src/Cayd.Random.Extensions/Extensions/NextByte.cs
+++ src/Cayd.Random.Extensions/Extensions/NextByte.cs
@@ -5,7 +5,7 @@
         /// <summary>
         /// Returns a non-negative random 8-bit integer.
         /// </summary>
-        /// <returns>A 8-bit unsigned integer that is between <see cref="byte.MinValue"/> and <see cref="byte.MaxValue"/>.</returns>
+        /// <returns>An 8-bit unsigned integer that is between <see cref="byte.MinValue"/> and <see cref="byte.MaxValue"/>.</returns>
         public static byte NextByte(this System.Random random)
             => (byte)random.Next(byte.MinValue, byte.MaxValue);
 
@@ -15,7 +15,7 @@
         /// <param name="minValue">The inclusive lower bound of the random number returned.</param>
         /// <param name="maxValue">The exclusive lower bound of the random number returned. maxValue must be greater than or equal to minValue.</param>
         /// <returns>
-        /// A 8-bit unsigned integer greater than or equal to <paramref name="minValue"/> and less than <paramref name="maxValue"/>; that is, the range of return values includes <paramref name="minValue"/>
+        /// An 8-bit unsigned integer greater than or equal to <paramref name="minValue"/> and less than <paramref name="maxValue"/>; that is, the range of return values includes <paramref name="minValue"/>
         /// but not <paramref name="maxValue"/>. If minValue equals <paramref name="maxValue"/>, <paramref name="minValue"/> is returned.
         /// </returns>
         /// <exception cref="ArgumentOutOfRangeException"><paramref name="minValue"/> is greater than <paramref name="maxValue"/>.</exception>
