f27a0ae426666e644ca95826d95a9f7649ef1c42
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-11 23:05:49 +0200

    add random sbyte generator

---
+++ src/Cayd.Random.Extensions/Extensions/NextSByte.cs
@@ -0,0 +1,25 @@
+﻿namespace Cayd.Random.Extensions
+{
+    public static partial class RandomExtensions
+    {
+        /// <summary>
+        /// Returns a random 8-bit integer.
+        /// </summary>
+        /// <returns>An 8-bit signed integer that is between <see cref="SByte.MinValue"/> and <see cref="SByte.MaxValue"/>.</returns>
+        public static sbyte NextSByte(this System.Random random)
+            => (sbyte)random.Next(SByte.MinValue, SByte.MaxValue);
+
+        /// <summary>
+        /// Returns a random 8-bit integer within a specified range.
+        /// </summary>
+        /// <param name="minValue">The inclusive lower bound of the random number returned.</param>
+        /// <param name="maxValue">The exclusive lower bound of the random number returned. maxValue must be greater than or equal to minValue.</param>
+        /// <returns>
+        /// An 8-bit signed integer greater than or equal to <paramref name="minValue"/> and less than <paramref name="maxValue"/>; that is, the range of return values includes <paramref name="minValue"/>
+        /// but not <paramref name="maxValue"/>. If minValue equals <paramref name="maxValue"/>, <paramref name="minValue"/> is returned.
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="minValue"/> is greater than <paramref name="maxValue"/>.</exception>
+        public static sbyte NextSByte(this System.Random random, sbyte minValue, sbyte maxValue)
+            => (sbyte)random.Next(minValue, maxValue);
+    }
+}
---
+++ test/UnitTests/Cayd.Random.Extensions.Test.Unit/Extensions/NextSByteTest.cs
@@ -0,0 +1,31 @@
+﻿namespace Cayd.Random.Extensions.Test.Unit.Extensions
+{
+    public class NextSByteTest
+    {
+        [Fact]
+        public void NextSByte_WhenRangeIsNotGiven_ShouldReturnValueWithinSByteRangeWithSameType()
+        {
+            // Act
+            var result = System.Random.Shared.NextSByte();
+
+            // Assert
+            Assert.IsType<sbyte>(result);
+            Assert.True(result >= SByte.MinValue, $"Result: {result}, Min Value: {SByte.MinValue}");
+            Assert.True(result <= SByte.MaxValue, $"Result: {result}, Max Value: {SByte.MaxValue}");
+        }
+
+        [Theory]
+        [InlineData(-10, 10)]
+        [InlineData(-128, 127)]
+        public void NextSByte_WhenRangeIsGiven_ShouldReturnValueWithinRangeWithSameType(sbyte min, sbyte max)
+        {
+            // Act
+            var result = System.Random.Shared.NextSByte(min, max);
+
+            // Assert
+            Assert.IsType<sbyte>(result);
+            Assert.True(result >= min, $"Result: {result}, Min Value: {min}");
+            Assert.True(result <= max, $"Result: {result}, Max Value: {max}");
+        }
+    }
+}
