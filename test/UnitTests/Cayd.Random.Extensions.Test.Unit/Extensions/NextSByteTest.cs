4b6e2fc07f9c89716ad1dae7dd309d309960c18c
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-12 11:57:28 +0200

    add maxValue overload method for nextsbyte

--- test/UnitTests/Cayd.Random.Extensions.Test.Unit/Extensions/NextSByteTest.cs
+++ test/UnitTests/Cayd.Random.Extensions.Test.Unit/Extensions/NextSByteTest.cs
@@ -35,5 +35,21 @@ namespace Cayd.Random.Extensions.Test.Unit.Extensions
             Assert.True(result >= min, $"Result: {result}, Min Value: {min}");
             Assert.True(result <= max, $"Result: {result}, Max Value: {max}");
         }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(sbyte.MaxValue)]
+        public void NextSByte_WhenOnlyMaxValueIsGiven_ShouldReturnValueBetweenZeroAndMaxWithSameType(sbyte max)
+        {
+            // Arrange
+            var rnd = new System.Random();
+
+            // Act
+            var result = rnd.NextSByte(max);
+
+            // Assert
+            Assert.IsType<sbyte>(result);
+            Assert.True(result >= 0 && result <= max, $"Result: {result}, Max Value: {max}");
+        }
     }
 }
